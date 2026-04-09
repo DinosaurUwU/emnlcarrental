7de9cb92faad5f121d8aceeda55dd505bf39a4c2
DinosaurUwU <Dinosaur1billion@gmail.com>
2026-04-09 18:03:25 +0800

    Update - 2026-04-09 18:03:25

--- src/app/lib/firebase.js
+++ src/app/lib/firebase.js
@@ -1,7 +1,7 @@
 // firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore, enableIndexedDbPersistence } from "firebase/firestore";
+import { initializeFirestore, CACHE_SIZE_UNLIMITED } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCLNlQG4-16KNzQpFdC4L2fFhtyVEiSrkY",
@@ -14,21 +14,16 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-export const db = getFirestore(app);
 
-// Enable offline persistence
-enableIndexedDbPersistence(db).catch((err) => {
-  if (err.code === "failed-precondition") {
-    console.warn(
-      "Multiple tabs open, persistence can only be enabled in one tab at a time.",
-    );
-  } else if (err.code === "unimplemented") {
-    console.warn("The current browser does not support persistence.");
-  }
+// Initialize Firestore with unlimited local cache
+const db = initializeFirestore(app, {
+  cacheSizeBytes: CACHE_SIZE_UNLIMITED
 });
+
+const auth = getAuth(app);
 
 const provider = new GoogleAuthProvider();
 provider.setCustomParameters({ prompt: "select_account" });
 
 export { auth, provider, app };
+export { db };
