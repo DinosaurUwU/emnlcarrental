5a3bb8d10715616fb276c86ea82d0af4105cd317
DinosaurUwU <Dinosaur1billion@gmail.com>
2026-04-10 23:20:40 +0800

    Update - 2026-04-10 23:20:40

--- commit.sh
+++ commit.sh
@@ -11,3 +11,11 @@ git commit -m "Update - $timestamp"
 
 # Push to GitHub (change 'main' if your branch is different)
 git push origin main
+
+# Add client's repo as remote if not exists, then push
+if ! git remote | grep -q "client"; then
+  git remote add client https://github.com/Emnl-Car-Rental-Services/emnlcarrentalservices.git
+fi
+
+# Push to client (excluding firebase.js)
+git push client main
