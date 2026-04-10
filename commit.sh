128fa6956daec5bd7705ca5496563976e08dc648
DinosaurUwU <Dinosaur1billion@gmail.com>
2026-04-10 23:15:51 +0800

    Update - 2026-04-10 23:15:51

--- commit.sh
+++ commit.sh
@@ -3,8 +3,8 @@
 # Get the current date and time
 timestamp=$(date +"%Y-%m-%d %H:%M:%S")
 
-# Add all changes
-git add .
+# Add all changes EXCEPT firebase.js
+git add --all -- ':!src/app/lib/firebase.js'
 
 # Commit with a default message
 git commit -m "Update - $timestamp"
